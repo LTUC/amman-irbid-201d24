885dc6fd8c0908910d47b06b7ab3f70d9a06f8f4
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-29 13:22:37 +0300

    create function for p2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { 
     let sum = a+b;
-    return[sum , `The sum of ${a} and ${b} is ${sum}.`]
+    return[sum , `The sum of ${a} and ${b} is ${sum}.`];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -27,12 +27,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) { 
+    let multiply = a*b;
+    return[multiply,`The product of ${a} and ${b} is ${multiply}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
