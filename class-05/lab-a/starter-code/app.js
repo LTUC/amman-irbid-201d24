9c144f0b87d849323a01ad344428147dab3d228f
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-29 13:33:47 +0300

    add p3 to solve problem 3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -59,7 +59,7 @@ function sumAndMultiply(a, b, c) {
     return[sum2, multiply2, `${a} and ${b} and ${c} sum to ${sum2}.`, `The product of ${a} and ${b} and ${c} is ${multiply2}.` ]
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
+//  Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
