78ac2efb5f74c5b757b8d01606a303130948b58a
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-05-09 11:43:38 +0300

    errors

---
+++ class-10/errors-demo/app.js
@@ -0,0 +1,47 @@
+'use strict';
+//syntaxError typo
+// if(){
+
+// }
+
+// if{ grade > 90 }(
+//     console.log('pass');
+// )
+// let user Name = 'shihab';
+
+// console.log(user Name);
+// let x = 10;
+// let x = 7;
+
+
+
+//reference error
+// let montaser;//undefined
+// console.log(montaser);//is not defined
+
+// call();//call is not defined
+
+//typeError 
+// let stdName = 'sondos';
+// // stdName.push('aljawhary');
+// stdName = stdName + ' aljawhary';
+// console.log(stdName);
+
+// const color = 'blue';
+// color = 'black';
+// console.log(color);
+
+// rangeError
+let grades = [];
+// grades.length = -1;
+// grades.length = 99 ** 99;
+
+
+let x = 10;
+// let x = 7;
+function check() {
+    let x = 4;
+    console.log(x);
+}
+check();
+console.log(x);
